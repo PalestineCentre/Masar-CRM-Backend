2ae18474d82569062b70a1767d219b4b860cf71f
ِAmjad <palestinecentre.it@gmail.com>
2026-06-06 17:20:03 +0200

    fix: add missing mongoose import

--- models/customer.js
+++ models/customer.js
@@ -1,5 +1,6 @@
 // models/Customer.js
 // تأكد من وجود هذه الحقول في موديل MongoDB الخاص بك
+import mongoose from 'mongoose';
 const customerSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
